da5b316fc02eca3dd07c781be81b7c4ea1e1616a
Salanky <titque1107@gmail.com>
2026-03-26 01:00:19 +0700

    feat(auction): update domain model with auction property

---
+++ src/AuctionStatus.java
@@ -0,0 +1,7 @@
+public enum AuctionStatus {
+    OPEN,       // Chưa bắt đầu
+    RUNNING,    // Đang diễn ra
+    FINISHED,   // Đã kết thúc
+    PAID,       // Đã thanh toán
+    CANCELED    // Bị hủy
+}
