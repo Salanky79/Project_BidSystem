8033544079390dbf03c36a9e4e5d5bdbc3ae68f6
LHT <225682344+tung1234yb-prog@users.noreply.github.com>
2026-03-26 17:18:40 +0700

    project structure

--- src/AuctionStatus.java
+++
@@ -1,7 +0,0 @@
-public enum AuctionStatus {
-    OPEN,       // Chưa bắt đầu
-    RUNNING,    // Đang diễn ra
-    FINISHED,   // Đã kết thúc
-    PAID,       // Đã thanh toán
-    CANCELED    // Bị hủy
-}
