08fb71374b3169f2342bcd33237eb538c064dd23
LHT <225682344+tung1234yb-prog@users.noreply.github.com>
2026-04-04 13:26:04 +0700

    fix: fix admin.java

--- server/src/models/user/Admin.java
+++ server/src/models/user/Admin.java
@@ -2,7 +2,7 @@ package src.models.user;
 
 import Enum.Role;
 
-public class Admin{
+public class Admin extends User{
     public Admin(String username, String password){
         this.username = username;
         this.password = password;
