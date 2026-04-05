8912cb7739442a7ff98d60ca87f5203705fd047d
Xuan Tan <Xtan2k7@gmail.com>
2026-04-05 16:34:57 +0700

    feat(giao dien client): Them logic chuyen giao dien Login-Home-Item

---
+++ giao dien client/src/main/java/com/example/demo1/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void handleLogin() {
+    }
+}
